4de2ecef10ce67bcaf0b2449c03b8715de3e0a33
Sylvain Nadeau <sylvain.nadeau@nexio.com>
2021-03-22 16:37:15 -0400

    Reduce amount of BigInt ops by chunking the input array

--- bvo.js
+++ bvo.js
@@ -1,18 +1,30 @@
 var zero = BigInt(0)
 var one = BigInt(1)
 var two = BigInt(2)
+var range = 10
+var bigval = new Array(2**range)
+for (var i = 0; i < 2**range; i++)
+	bigval[i] = BigInt(i)
+var bvcount = 0
+
 class BinaryVector {
     // create the binary vector from an array of booleans
 	// NOTE: It is assumed that all bv sizes to be equal
 	constructor(v,s=0){
+		bvcount++
 		if (s){
 			this.rep = v
 			this.size = s
-		} else if (v) {
-			this.rep = zero
+		} else if (v && v.length) {
 			this.size = v.length
-			for (var i = 0; i < this.size; i++) 
-				this.rep += v[i] ? two ** BigInt(i) : zero	
+			this.rep = zero
+			for (var i = 0; i < this.size; i+=range){
+				var c = 0
+				var max = Math.min(i+range,this.size)
+				for (var ii = i; ii < max; ii++)
+					c += v[ii] ? 1<<(ii-i) : 0
+				this.rep += two ** BigInt(i) * bigval[c]
+			}
 		} else {
 			this.rep = zero
 			this.size = 0;
@@ -22,13 +34,13 @@ class BinaryVector {
 		return this.rep==bv.rep
     }
     intersection(bv){
-		return new BinaryVector(this.rep&bv.rep, Math.max(this.size,bv.size))
+		return this.size*bv.size==0 ? bvzero : new BinaryVector(this.rep&bv.rep, Math.max(this.size,bv.size))
     }
     union(bv){
-		return new BinaryVector(this.rep|bv.rep, Math.max(this.size,bv.size))
+		return this.size+bv.size>0 ? bvzero : new BinaryVector(this.rep|bv.rep, Math.max(this.size,bv.size))
     }
 	inverse(){
-		return new BinaryVector(two ** BigInt(this.size) - one - this.rep, this.size)
+		return new BinaryVector(two ** BigInt(i) - one - this.rep, this.size)
 	}
 	// Not really the difference
 	// Returns true when A=true and B=false
@@ -37,14 +49,7 @@ class BinaryVector {
 		return this.intersection(bv.inverse())
     }
     get(i){ // return boolean
-		var bigi = two ** BigInt(i)
-		return this.rep >= bigi && this.rep / bigi % two == 1
-    }
-    toString(){
-		return this.rep.toString()
-		/*var re = " ";
-		for (var i = 0; i < this.size; i++)
-			re += this.get(i) + " "
-		return re;*/
+		return this.rep&(two ** BigInt(i))
     }
 }
+var bvzero = new BinaryVector([])
